6216e817d98178e431798568d3ad6ea28f6c7f07
Yuqi.Wang <yuqi.wang@thoughtworks.com>
2020-07-07 14:45:39 +0800

    Finished Header's style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import "./assets/font_icons/fonts.css"
 import {Header} from "./components/Header/Header";
 import {Menu} from "./components/Menu/Menu";
 import {Content} from "./components/Content/Content";
@@ -7,12 +8,12 @@ import {Footer} from "./components/Footer/Footer";
 
 function App() {
   return (
-      <>
+      <div className="App">
         <Header />
         <Menu />
         <Content />
         <Footer />
-      </>
+      </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../../assets/logo/logo.svg"
 import avatar from "../../assets/logo/avatar.jpg"
+import './Header.scss'
 
 export class Header extends React.Component {
     render() {
         return (
-            <header>
+            <header className="header">
                 <img className="logo" src={logo} alt="logo" />
                 <img className="avatar" src={avatar} alt="avatar" />
             </header>
