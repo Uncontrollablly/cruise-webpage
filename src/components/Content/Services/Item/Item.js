f82fd582319835a4ee33668a501986145979a403
Yuqi.Wang <yuqi.wang@thoughtworks.com>
2020-07-10 12:43:08 +0800

    Initialized Item's style

--- src/components/Content/Services/Item/Item.js
+++ src/components/Content/Services/Item/Item.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import cent_os from '../../../../assets/os_icons/cent_os.png'
+import debin from '../../../../assets/os_icons/debin.png'
+import suse from '../../../../assets/os_icons/suse.png'
+import ubuntu from '../../../../assets/os_icons/ubuntu.png'
+import windows from '../../../../assets/os_icons/windows.png'
 
 export class Item extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            cent_os,
+            debin,
+            suse,
+            ubuntu,
+            windows,
+        }
+    }
+
+    createSourcesList = () =>
+        this.props.resources.map((resource) =>
+            <li>
+                <label>
+                    {resource}
+                    <button className="icon-trash"/>
+                </label>
+            </li>
+        );
+
     render() {
         return (
-            <section className="item">
-                {this.props.children}
-            </section>
+            <div className="item">
+                <img
+                    src={this.state[this.props.os]}
+                    alt={this.props.os}
+                />
+                <div className="item-content">
+                    <div className="first-line">
+                        <div className="icon-desktop">
+                            {this.props.name}
+                        </div>
+                        <div className="status">
+                            {this.props.status}
+                        </div>
+                        <div className="icon-info">
+                            {this.props.ip}
+                        </div>
+                        <div className="icon-folder">
+                            {this.props.location}
+                        </div>
+                    </div>
+
+                    <div className="second-line">
+                        <ul>
+                            <li>
+                                <button className="icon-plus" />
+                            </li>
+                            {this.createSourcesList()}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/components/Content/Services/Services.js
+++ src/components/Content/Services/Services.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import windows from '../../../assets/os_icons/windows.png'
-import ubuntu from '../../../assets/os_icons/ubuntu.png'
-import debin from '../../../assets/os_icons/debin.png'
 import {Item} from './Item/Item';
 
 export class Services extends React.Component {
@@ -9,134 +6,14 @@ export class Services extends React.Component {
     render() {
         return (
             <section>
-                <Item>
-                    <img src={windows} alt="windows" />
-                    <div className="first-line">
-                        <div className="url">
-                            <i className="icon-desktop"></i>
-                            <span>bjstdmngbgr01.thoughtworks.com</span>
-                        </div>
-                        <div className="state-idle">
-                            idle
-                        </div>
-                        <div className="ip">
-                            <i className="icon-info"></i>
-                            <span>192.168.1.102</span>
-                        </div>
-                        <div className="path"><i className="icon-folder"></i>
-                            <span>/var/lib/cruise-agent</span>
-                        </div>
-                    </div>
-                    <div className="second-line">
-                        <ul>
-                            <li>
-                                <i className="icon-plus"></i>
-                            </li>
-                            <li>
-                                Firefox
-                                <i className="icon-trash"></i>
-                            </li>
-                            <li>
-                                Safari
-                                <i className="icon-trash"></i>
-                            </li>
-                            <li>
-                                Ubuntu
-                                <i className="icon-trash"></i>
-                            </li>
-                            <li>
-                                Chrome
-                                <i className="icon-trash"></i>
-                            </li>
-                        </ul>
-                    </div>
-                </Item>
-
-                <Item>
-                    <img src={ubuntu} alt="ubuntu" />
-                        <div className="first-line">
-                            <div className="url">
-                                <i className="icon-desktop"></i>
-                                <span>bjstdmngbgr10.thoughtworks.com</span>
-                            </div>
-                            <div className="state-building">
-                                building
-                            </div>
-                            <div className="ip">
-                                <i className="icon-info"></i>
-                                <span>192.168.1.80</span>
-                            </div>
-                            <div className="path"><i className="icon-folder"></i>
-                                <span>/var/lib/cruise-agent</span>
-                            </div>
-                        </div>
-                        <div className="second-line">
-                            <ul>
-                                <li>
-                                    <i className="icon-plus"></i>
-                                </li>
-                                <li>
-                                    Firefox
-                                    <i className="icon-trash"></i>
-                                </li>
-                                <li>
-                                    Safari
-                                    <i className="icon-trash"></i>
-                                </li>
-                            </ul>
-                            <div>
-                                <i className="icon-deny"></i>
-                                <button type="button">Deny</button>
-                            </div>
-                        </div>
-                </Item>
-
-                <Item>
-                    <img src={debin} alt="debin" />
-                        <div className="first-line">
-                            <div className="url">
-                                <i className="icon-desktop"></i>
-                                <span>bjstdmngbgr11.thoughtworks.com</span>
-                            </div>
-                            <div className="state-building">
-                                building
-                            </div>
-                            <div className="ip">
-                                <i className="icon-info"></i>
-                                <span>192.168.1.117</span>
-                            </div>
-                            <div className="path"><i className="icon-folder"></i>
-                                <span>/var/lib/cruise-agent</span>
-                            </div>
-                        </div>
-                        <div className="second-line">
-                            <ul>
-                                <li>
-                                    <i className="icon-plus"></i>
-                                </li>
-                                <li>
-                                    Firefox
-                                    <i className="icon-trash"></i>
-                                </li>
-                                <li>
-                                    Safari
-                                    <i className="icon-trash"></i>
-                                </li>
-                                <li>
-                                    Ubuntu
-                                    <i className="icon-trash"></i>
-                                </li>
-                                <li>
-                                    Chrome
-                                    <i className="icon-trash"></i>
-                                </li>
-                            </ul>
-                            <div>
-                                <i className="icon-deny"></i>
-                                <button type="button">Deny</button>
-                            </div>
-                        </div>
-                </Item>
+                <Item
+                    os='windows'
+                    name='bjstdmngbgr01.thoughtworks.com'
+                    status='idle'
+                    ip='192.168.1.102'
+                    location='/var/lib/cruise-agent'
+                    resources={['Firfox', 'Safari', 'Ubuntu', 'Chrome']}
+                />
             </section>
         );
     }
