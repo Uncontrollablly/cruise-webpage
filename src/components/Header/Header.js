e53fb1145be3da94fa297320f8b2a88b076da614
Yuqi.Wang <yuqi.wang@thoughtworks.com>
2020-07-17 15:00:15 +0800

    Changed webpage layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import avatar from '../../assets/logo/avatar.jpg'
 export class Header extends React.Component {
     render() {
         return (
-            <header className="header">
+            <header>
                 <img className="logo" src={logo} alt="logo" />
                 <img className="avatar" src={avatar} alt="avatar" />
             </header>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,12 +4,14 @@ import {History} from '../History/History';
 export class Menu extends React.Component {
     render() {
         return (
-            <aside className="Menu">
-                <button className="icon-dashboard">DASHBOARD</button>
-                <button className="icon-sitemap">AGENT</button>
-                <button className="icon-boat">MY CRUISE</button>
-                <button className="icon-life-bouy">HELP</button>
-                <History/>
+            <aside className="menu">
+                <div className='menu-container'>
+                    <button className="icon-dashboard">DASHBOARD</button>
+                    <button className="icon-sitemap">AGENT</button>
+                    <button className="icon-boat">MY CRUISE</button>
+                    <button className="icon-life-bouy">HELP</button>
+                    <History/>
+                </div>
             </aside>
         );
     }
